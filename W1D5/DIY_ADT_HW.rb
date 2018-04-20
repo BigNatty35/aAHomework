5c3cf7f90a22d4fcda4043c3a00c2b986c1f19b1
Carey Johnson <johnson.careyj@gmail.com>
2018-04-19 21:33:56 -0700

    Created Map class

--- W1D5/DIY_ADT_HW.rb
+++ W1D5/DIY_ADT_HW.rb
@@ -38,3 +38,34 @@ class Queue
   end
 
 end
+
+
+class Map
+  def initialize
+    @map = []
+  end
+
+  def assign(key, value)
+    @map << [key, value]
+  end
+
+  def lookup(key)
+    @map.each do |pair|
+      if pair.include?(key)
+        return pair[1]
+      end
+    end
+  end
+
+  def remove(key)
+    @map.each do |pair|
+     @map.delete(pair) if pair.include?(key)
+    end
+    @map
+  end
+
+  def show
+    @map
+  end
+
+end
