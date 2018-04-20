f770da1191356650f0a7bf2744bfbec0afda150e
Carey Johnson <johnson.careyj@gmail.com>
2018-04-19 21:11:20 -0700

    Stack class created

---
+++ W1D5/DIY_ADT_HW.rb
@@ -0,0 +1,20 @@
+class Stack
+  attr_accessor
+
+  def initialize
+    @stack = []
+  end
+
+  def add(el)
+    @stack << el
+  end
+
+  def remove
+    @stack.pop
+  end
+
+  def show
+    @stack
+  end
+
+end
